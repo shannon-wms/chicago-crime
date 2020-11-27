971cfa2fd82e3e000dd8ad989cbe6933563e7002
Shannon Williams <shannon.williams@bristol.ac.uk>
2020-11-27 15:52:57 +0000

    Load tidyverse

--- eda/sf_testing.R
+++ eda/sf_testing.R
@@ -1,5 +1,6 @@
 library(sf)
 library(raster)
+library(tidyverse)
 Crime_2019 <- read_csv("data/crime-2019.csv")
 newCrime <- na.omit(Crime_2019)
 
@@ -9,16 +10,13 @@ plot(st_geometry(bounds))
 
 pnts_sf <- st_as_sf(newCrime, coords = c('Longitude', 'Latitude'), crs = st_crs(bounds))
 
-
 pnts <- pnts_sf %>% mutate(
   intersection = as.integer(st_intersects(geometry, bounds))
   , area = if_else(is.na(intersection), '', bounds$PRI_NEIGH[intersection])
-) 
-
-
-plot(pnts_sf)
+)
 
 pnts
 data.frame(table(pnts$intersection))
 bounds$total_crime <- data.frame(table(pnts$intersection))$Freq
 plot(bounds["total_crime"])
+
