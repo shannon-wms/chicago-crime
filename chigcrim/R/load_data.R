ee923bf54ce8e54d94ca1bcbbdddd2967e15a2c5
Shannon Williams <shannon.williams@bristol.ac.uk>
2020-12-09 11:36:25 +0000

    Change column names to lowercase and update documentation.

--- chigcrim/R/load_data.R
+++ chigcrim/R/load_data.R
@@ -1,18 +1,32 @@
+# Function for downloading Chicago Crime data by year from Socrata API
+#' @import RSocrata
+#' @import dplyr
+#' @import magrittr
+NULL
+
+#' Download Chicago Crime data
+#' 
+#' @description
+#' Returns a data frame of Chicago Crime data from specified year, or entire dataset
+#' from 2001 to present if no year is specified.
+#' @param year integer in 2001:2020 specifying the desired year.
+#' @return tibble Data frame of Chicago Crime data.
+
 load_data <- function(year = NULL) {
   # Only accept valid years
-  stopifnot(year %in% 2001:2020)
+  if(!(year %in% 2001:2020)) return("Please choose a year between 2001 and 2020.")
   base_url <- "https://data.cityofchicago.org/resource/ijzp-q8t2.csv"
   # Download entire dataset if year not specified
   full_url <- ifelse(!is.null(year), paste0(base_url, "?Year=", as.character(year)), base_url)
   df <- read.socrata(full_url, app_token = "avsRhaZaZTqeJOGhBuFJieRzJ") %>% tibble()
-  # Edit column names
+  # Convert dots in column names to underscore
   colnames(df) %<>% strsplit(split = "\\.") %>% lapply(paste, collapse = "_")
-  # Convert character columns (excluding Case_Number and Block) to factor or logical
-  df %>% type_convert(col_types = list(IUCR = col_factor(),
-                                       Primary_Type = col_factor(),
-                                       Description = col_factor(),
-                                       Location_Description = col_factor(),
-                                       Arrest = col_logical(),
-                                       Domestic = col_logical()))
+  # Convert character columns (excluding case_number and block) to factor or logical
+  df %<>% type_convert(col_types = list(iucr = col_factor(),
+                                        primary_type = col_factor(),
+                                        description = col_factor(),
+                                        location_description = col_factor(),
+                                        arrest = col_logical(),
+                                        domestic = col_logical()))
   return(df)
 }
