dc429149eb2d8fce86b63ec3bfef5c01228a7f64
Daniel Ward <danielward27@outlook.com>
2021-01-16 19:50:06 +0000

    Dan update (added CV functions for R6)

--- chigcrim/R/utils.R
+++ chigcrim/R/utils.R
@@ -49,12 +49,20 @@ parse_X <- function(X){
 #' @return tibble Data frame of Chicago Crime data.
 load_data <- function(year = NULL, strings_as_factors = TRUE,
                       drop_location = TRUE, na_omit = FALSE) {
-  # Only accept valid years
-  if(!(year %in% 2001:2020)) return("Please choose a year between 2001 and 2020.")
+
+  # Only accept NULL or valid years
+  if (!is.null(year)){
+    if(!(year %in% 2001:2020)){
+      return("Please choose a year between 2001 and 2020.")
+    }
+  }
+
   base_url <- "https://data.cityofchicago.org/resource/ijzp-q8t2.csv"
+
   # Download entire dataset if year not specified
   full_url <- ifelse(!is.null(year), paste0(base_url, "?Year=", as.character(year)), base_url)
   df <- read.socrata(full_url, app_token = "avsRhaZaZTqeJOGhBuFJieRzJ") %>% tibble()
+
   # Omit observations with NA values
   if (na_omit) df %<>% na.omit()
   # Convert dots in column names to underscore
@@ -110,4 +118,83 @@ otherise <- function(string_vec, n_threshold, print_summary = TRUE){
 yday_float = function(timestamp){
   yday(timestamp) + hour(timestamp)/24 + minute(timestamp)/(24*60)
 }
+
+
+
+#' Indexed cross-validation for R6 class
+#' Computes cross validation using specific indexes (rows) of the data set
+#' as the test set.
+#' @param object R6 class with fit and predict methods.
+#' @param X Matrix of data.
+#' @param y Vector of observations.
+#' @param error Function to assess error.
+#' @param idxs Vector of indices corresponding with test data.
+#'
+#' @return Average error across the folds.
+cv_R6_idxs <- function(object, X, y, error, idxs){
+  X_train <- X[-idxs, , drop = FALSE]
+  X_test <- X[idxs, , drop = FALSE]
+  y_train <- y[-idxs]
+  y_test <- y[idxs]
+
+  object$fit(X_train, y_train)
+  y_hat <- object$predict(X_test)
+
+  error(y_test, y_hat)
+}
+
+#' Cross-validation error for R6 class
+#' Standard cross validation without folds.
+#' @param object R6 class with fit and predict methods
+#' @param X Matrix of data
+#' @param y Vector of observations
+#' @param error Function to calculate error (taking y and y_hat)
+#' @param test_size Proportion of data to use for testing
+#'
+#' @return Result of error function
+#' @export
+#' @examples
+#' n <- 50
+#' X <- matrix(runif(n, -2,4), nrow=n, ncol=1)
+#' y <- as.vector(sin(X)) + rnorm(n, sd = 0.3)
+#' kr <- KernelRidge$new("rbf", lambda = 1, 3)
+#' cv_R6(kr, X, y, squared_error_loss, 0.2)
+cv_R6 <- function(object, X, y, error, test_size){
+  n <- nrow(X)
+  idxs <- sample(1:n, round(test_size*n))
+  cv_R6_idxs(object, X, y, error, idxs)
+}
+
+
+#' K-fold cross validation for R6 class
+#'
+#' @param object R6 class with fit and predict methods
+#' @param X Matrix of data
+#' @param y Vector of observations
+#' @param error Function to calculate error (taking y and y_hat)
+#' @param k Number of folds to use
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' n <- 50
+#' X <- matrix(runif(n, -2,4), nrow=n, ncol=1)
+#' y <- as.vector(sin(X)) + rnorm(n, sd = 0.3)
+#' kr <- KernelRidge$new("rbf", lambda = 1, 3)
+#' cv_R6_k_fold(kr, X, y, squared_error_loss, 5)
+cv_R6_k_fold <- function(object, X, y, error, k){
+  n <- nrow(X)
+  parts <- split(sample(1:n), 1:k)
+
+  errors <- c()
+  for (idxs in parts){
+    err <- cv_R6_idxs(object, X, y, error, idxs)
+    errors <- c(errors, err)
+  }
+  mean(errors)
+}
+
+
+
 
