5910262e12ef85176ae9bb18703c738922e9e0f6
Shannon Williams <shannon.williams@bristol.ac.uk>
2021-01-22 23:15:52 +0000

    Move get_kernel_function to private and tidy up code.

--- chigcrim/R/regression.R
+++ chigcrim/R/regression.R
@@ -24,21 +24,22 @@ NULL
 #' @field A First hyperparameter (see description).
 #' @field B Second hyperparameter (see description).
 #' @field kernel_function The kernel function.
-#' @field n Integer length of training data.
-#' @field inv_big_k_lambda_y_train Matrix containing (K + lambda I)^-1 . y_train
+#' @field n Length of training data.
+#' @field pred_vector Vector containing (K + lambda I)^-1 * y_train, the 
+#' pre-computed component of the prediction function.
 #' @field X_train Training X matrix.
 #' @field y_train Training y vector.
 #' @field X_test Testing X matrix.
 #' @field y_test Testing y vector.
-#' @field prediction The predicted values.
+#' @field predictions The predicted values.
 #' @export
 #' @examples
 #' n <- 200
 #' n_test <- 100
-#' X_train <- matrix(runif(n, -2,4), nrow=n, ncol=1)
-#' X_test <- matrix(seq(from =-2, to=4, length.out = n_test), nrow=n_test, ncol=1)
+#' X_train <- matrix(runif(n, -2, 4), nrow = n, ncol = 1)
+#' X_test <- matrix(seq(from = -2, to = 4, length.out = n_test), nrow = n_test, ncol = 1)
 #' y_train <- as.vector(sin(X_train)) + rnorm(n, sd = 0.3)
-#' kr <- KernelRidge$new("rbf", lambda = 1, 3)
+#' kr <- KernelRidge$new("rbf", lambda = 1, A = 3)
 #' kr$fit(X_train, y_train)
 #' y_hat_rbf <- kr$predict(X_test)
 KernelRidge <- R6Class("KernelRidge", public = list(
@@ -48,12 +49,12 @@ KernelRidge <- R6Class("KernelRidge", public = list(
   B = "numeric",
   kernel_function = "function",
   n = "integer",
-  inv_big_k_lambda_y_train = "matrix",
+  pred_vector = "vector",
   X_train = "matrix",
   X_test = "matrix",
   y_train = "vector",
   y_test = "vector",
-  prediction = "vector",
+  predictions = "vector",
 
   #' @description
   #' Create new KernelRidge object.
@@ -68,68 +69,47 @@ KernelRidge <- R6Class("KernelRidge", public = list(
     self$B <- B
 
     private$check_params()
-    self$kernel_function <- self$get_kernel_function()
-  },
-
-
-  #' @description
-  #' Gives the kernel function with hyperparameters set.
-  get_kernel_function = function(){
-    if (self$kernel == "linear"){
-      return(private$linear_kernel)
-    }
-
-    else if (self$kernel == "polynomial"){
-      return(function(x1, X2) private$polynomial_kernel(x1, X2, self$A, self$B))
-    }
-
-    else if (self$kernel == "rbf"){
-      return(function(x1, X2) private$rbf_kernel(x1, X2, self$A))
-    }
+    self$kernel_function <- private$get_kernel_function()
   },
 
   #' @description
   #' Fits the big K matrix. n by n matrix of kernel evaluations on X_train.
   #' @param X_train Training X matrix.
-  #' @param y_train Training y vector
+  #' @param y_train Training y vector.
   fit = function(X_train, y_train){
     stopifnot(length(y_train) == nrow(X_train))
 
     if (self$kernel %in% c("linear", "polynomial")){
-      X_train <- cbind(1, X_train)  # RBF doesn't need bias term.
+      X_train <- cbind(1, X_train)  # RBF doesn't require bias term
     }
-
-    self$y_train <- y_train  # Not really used here, just for consistency
+    self$y_train <- y_train  # Include only for consistency
     self$X_train <- X_train
     self$n <- nrow(X_train)
     K <- matrix(nrow = self$n, ncol = self$n)
-
     for (j in 1:self$n){
       K[, j] <- self$kernel_function(X_train[j, ], X_train)
     }
     I <- diag(nrow = self$n, ncol = self$n)
-    self$inv_big_k_lambda_y_train <- solve(K + self$lambda*I, self$y_train)
+    self$pred_vector <- solve(K + self$lambda*I, self$y_train)
   },
 
   #' @description
   #' Predicts on an X matrix (test or train)
   #' @param X_test Test X matrix.
+  #' @return predictions A vector of predicted values.
   predict = function(X_test){
-
     if (self$kernel %in% c("linear", "polynomial")){
       X_test <- cbind(1, X_test)  # RBF doesn't need bias term.
     }
-
     self$X_test <- X_test
     predictions <- c()
-
     for (i in 1:nrow(X_test)){
       little_k <- self$kernel_function(X_test[i, ], self$X_train)
-      pred <- t(little_k) %*% self$inv_big_k_lambda_y_train
+      pred <- t(little_k) %*% self$pred_vector
       predictions <- c(predictions, pred)
     }
-    self$prediction <- predictions
-    return(predictions)
+    self$predictions <- predictions
+    return(self$predictions)
   }
 ), private = list(
   # Checks arguments are defined correctly
@@ -147,19 +127,34 @@ KernelRidge <- R6Class("KernelRidge", public = list(
                  "parameter, and B should not be defined"))
     }
   },
-
+  
+  #' Gives the kernel function with hyperparameters set.
+  get_kernel_function = function(){
+    if (self$kernel == "linear"){
+      return(private$linear_kernel)
+    }
+    
+    else if (self$kernel == "polynomial"){
+      return(function(x1, X2) private$polynomial_kernel(x1, X2, self$A, self$B))
+    }
+    
+    else if (self$kernel == "rbf"){
+      return(function(x1, X2) private$rbf_kernel(x1, X2, self$A))
+    }
+  },
+  
   #' Radial basis function kernel function (compares matrix rows to vector)
   rbf_kernel = function(x1, X2, bandwidth){
     difs <- t(X2)-x1
     dot_prods <- colSums(difs^2)
-    exp(-(sqrt(dot_prods))/(2*bandwidth^2))
+    exp(-(sqrt(dot_prods)) / (2 * bandwidth^2))
   },
-
+  
   #' Polynomial kernel function (compares matrix rows to vector)
   polynomial_kernel  = function(x1, X2, constant, degree){
     c((X2 %*% x1 + constant)^degree)
   },
-
+  
   #' Linear kernel function (compares matrix rows to vector)
   linear_kernel  = function(x1, X2){
     c(X2 %*% x1)
@@ -176,14 +171,14 @@ KernelRidge <- R6Class("KernelRidge", public = list(
 #' @export
 #'
 #' @examples
-#' squared_error_loss(c(1,2,3), c(2,3,2))
+#' squared_error_loss(c(1, 2, 3), c(2, 3, 2))
 squared_error_loss <- function(y_hat, y){
   mean((y_hat - y)^2)
 }
 
 #' Root mean squared error loss
 #'
-#' @param y_hat Vector of predictions.
+#' @param y_hat Vector of predicted values.
 #' @param y Vector of observed values.
 #'
 #' @return Float root mean squared error loss
@@ -196,7 +191,7 @@ rmse_loss <- function(y_hat, y){
 }
 
 #' R-Squared
-#'
+#' @description
 #' Calculates the R2 value.
 #'
 #' @param y_hat Vector of predictions.
@@ -215,7 +210,15 @@ r_squared <- function(y_hat, y){
 #'
 #' @description
 #' R6 class wrapper for the Poisson family of GAMs, fitted via `gam` from the package
-#' mgcv.
+#' **mgcv** and tailored for use with a subset of the Chicago Crime dataset. To initialise
+#' the object, the user supplies the variables over which to aggregate the data and, if 
+#' "community_area" is chosen as `region`, whether to include a neighbourhood list as part
+#' of the model. When `fit` is provided with data containing columns corresponding to 
+#' `region`, `time_period` and `crime_type` as well as a column "date", it will aggregate
+#' counts over these variables. It then fits a GAM with a cyclic cubic regression smoother for
+#' `time_period` and treats `region` and `crime_type` as factors (unless a neighbourhood list
+#' is included, in which case a Markov random field smoother is applied to `region`). For more
+#' details on model fitting methods, see **mgcv**.
 #'
 #' @field df_train The training dataset.
 #' @field df_test The test dataset.
@@ -225,20 +228,31 @@ r_squared <- function(y_hat, y){
 #' "ward" or "community".
 #' @field crime_type The crime type to include in the model as predictor. If `NULL`, 
 #' this will not be included in the model.
-#' @field include_nb Whether to include a neighbours list for the discrete spatial
-#' regions and then use a Markov random field smoother for fitting.
+#' @field include_nb If "community_area" is the specified region, whether to include a 
+#' neighbourhood list and then use a Markov random field smoother for fitting.
 #' @field filter_week If using weeks, whether to filter out week 53, as it
 #' typically has fewer than 7 days and can lead to spurious results.
 #' @field n_threads Number of threads to use when fitting the GAM in parallel.
-#' @field nbd_list Neighbourhood list for the specified region.
+#' @field nb_list Neighbourhood list for the community areas.
 #' @field count_train Count data for the training dataset, grouped by the specified
 #' time period, region, and (optional) crime type.
 #' @field count_test Count data for the test dataset, grouped by the specified time
 #' period, region, and (optional) crime type.
-#' @field gam_fitted The GAM, fitted using package mgcv.
+#' @field gam_fitted The GAM, fitted using package **mgcv**.
 #' @field fit_summary Summary of the fitted GAM.
 #' @field predictions Vector of predicted response values for the test dataset.
+#' 
 #' @export
+#' 
+#' @examples
+#' data <- load_data(2019, na_omit = TRUE)
+#' test_index <- sample(1:nrow(data), size = nrow(data) %/% 10)
+#' data_train <- data[-test_index, ]
+#' data_test <- data[test_index, ]
+#' pg <- PoissonGAM$new(time_period = "week", region = "community_area",
+#'                      crime_type = NULL, include_nb = FALSE, filter_week = TRUE)
+#' pg$fit(df_train = data_train, convert = TRUE, n_threads = 1)
+#' pred <- pg$predict(df_test = data_test, convert = TRUE)
 PoissonGAM <- R6Class("PoissonGAM", public = list(
   df_train = "data.frame",
   df_test = "data.frame",
@@ -248,7 +262,7 @@ PoissonGAM <- R6Class("PoissonGAM", public = list(
   include_nb = "logical",
   filter_week = "logical",
   n_threads = "integer",
-  nbd_list = "list",
+  nb_list = "list",
   count_train = "data.frame",
   count_test = "data.frame",
   gam_fitted = "list",
@@ -275,7 +289,7 @@ PoissonGAM <- R6Class("PoissonGAM", public = list(
     self$filter_week <- filter_week
 
     private$check_params()
-    if (include_nb) self$nbd_list <- private$get_nbd_list()
+    if (include_nb) self$nb_list <- private$get_nb_list()
   },
 
   #' @description
@@ -305,9 +319,9 @@ PoissonGAM <- R6Class("PoissonGAM", public = list(
     # Add region and neighbourhood list if required
     if (self$include_nb) {
       f <- formula(paste(deparse(f), "+ s(", self$region,  
-                         ", bs = 'mrf', xt = list(nb = self$nbd_list))"))
+                         ", bs = 'mrf', xt = list(nb = self$nb_list))"))
     } else f <- formula(paste(deparse(f), "+", self$region))
-    # Fit GAM using mgcv
+    # Fit GAM using mgcv::gam
     self$gam_fitted <- gam(f, data = self$count_train, family = "poisson",
                            control = ctrl, ...)
     self$fit_summary <- summary(self$gam_fitted)
@@ -317,6 +331,7 @@ PoissonGAM <- R6Class("PoissonGAM", public = list(
   #' @param df_test The training dataset.
   #' @param convert Whether the column `date` should be converted to instants
   #' or they are already present.
+  #' @return Vector of predicted values.
   predict = function(df_test = NULL, convert = FALSE) {
     if (!is.null(df_test)) {
       # Convert date column to instants if necessary
@@ -333,20 +348,22 @@ PoissonGAM <- R6Class("PoissonGAM", public = list(
     } else { # No test data, predict on training data
       self$predictions <- predict(self$gam_fitted, type = "response")
     }
+    return(self$predictions)
   }
 ), private = list(
-  # Check parameters are defined properly
+  #' Check parameters are defined properly
   check_params = function() {
     if (!self$time_period %in% c("week", "day", "month")) {
       stop('Time period must be one of "yday", "week" or "month".')
-    } else if (!self$region %in% c("beat", "community_area")) {
-      stop('Region must be one of "beat" or "community_area".')
-    } else if (!self$crime_type %in% c("primary_type", "description",
-                                       "fbi_code")) {
+    } else if (!self$region %in% c("beat", "ward", "district", "community_area")) {
+      stop('Region must be one of "beat", "ward", "district", or "community_area".')
+    } else if (!self$crime_type %in% c("primary_type", "description", "fbi_code")) {
       stop('Crime type must be one of "primary_type", "description", or "fbi_code".')
+    } else if (self$include_nb && !(self$region == "community_area")) {
+      stop('Neighbourhood list is compatible only with "community_area" type region.')
     }
   },
-  # Aggregate given dataset into count data
+  #' Aggregate given dataset into count data.
   get_count_data = function(df) {
     # Crime type data included
     if (!is.null(self$crime_type)) {
@@ -358,8 +375,7 @@ PoissonGAM <- R6Class("PoissonGAM", public = list(
                !!eval(self$region) := `get(self$region)`,
                !!eval(self$crime_type) := `get(self$crime_type)`) %>%
         mutate(!!eval(self$crime_type) := fct_relevel(get(self$crime_type), sort)) %>%
-        arrange(get(self$region), get(self$time_period), 
-                get(self$crime_type), year)
+        arrange(get(self$region), get(self$time_period), get(self$crime_type), year)
     } else { # Crime type data not included
       count_data <- df %>%
         mutate(!!eval(self$region) := factor(get(self$region))) %>%
@@ -370,19 +386,13 @@ PoissonGAM <- R6Class("PoissonGAM", public = list(
     }
     return(count_data)
   },
-  # Construct a neighbourhood list for beats or community areas
-  get_nbd_list = function() {
-    if (self$region == "beat") {
-      data("beat_bounds")
-      nbd_list <- poly2nb(beat_bounds, row.names = beat_bounds$beat)
-    }
-    else {
-      data("community_bounds")
-      nbd_list <- poly2nb(community_bounds, row.names = community_bounds$community)
-    }
+  #' Construct a neighbourhood list for community areas
+  get_nb_list = function() {
+    data("community_bounds")
+    nb_list <- poly2nb(community_bounds, row.names = community_bounds$community)
     # Name the rows in the list
-    names(nbd_list) <- attr(nbd_list, "region.id")
-    return(nbd_list)
+    names(nb_list) <- attr(nb_list, "region.id")
+    return(nb_list)
   }
 )
 )
